ac8b967cf8ef4d1515d4d4ddf2f61c2940cc21f6
Sasha Semivelichenko <recall.rpg@mail.ru>
2021-11-03 17:22:47 +0500

    initial commit

---
+++ app/src/main/java/com/example/cleanweatherapp/BaseApplication.kt
@@ -0,0 +1,5 @@
+package com.example.cleanweatherapp
+
+import android.app.Application
+
+class BaseApplication: Application()
---
+++ app/src/main/java/com/example/cleanweatherapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cleanweatherapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cleanweatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
